d78e11c4aad70878e56437859965b005b068da0d
kgazha <kgazha@gmail.com>
2021-01-30 15:15:43 +0500

    Add User model

---
+++ UserManager/UserManager/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManager.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public string Password { get; set; }
+    }
+}
