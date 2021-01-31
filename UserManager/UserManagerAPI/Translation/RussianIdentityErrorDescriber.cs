83ae3903aa20cc80d30c756971c6341cba074f4c
kgazha <kgazha@gmail.com>
2021-01-31 18:50:51 +0500

    Add transliteration for the username error

--- UserManager/UserManagerAPI/Translation/RussianIdentityErrorDescriber.cs
+++ UserManager/UserManagerAPI/Translation/RussianIdentityErrorDescriber.cs
@@ -9,6 +9,7 @@ namespace UserManagerAPI.Translation
     public class RussianIdentityErrorDescriber : IdentityErrorDescriber
     {
         public override IdentityError PasswordMismatch() { return new IdentityError { Code = nameof(PasswordMismatch), Description = "Неверный пароль." }; }
+        public override IdentityError InvalidUserName(string userName) { return new IdentityError { Code = nameof(InvalidUserName), Description = $"Имя пользователя '{userName}' должно содержать только буквы и цифры." }; }
         public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"Пароль должен иметь как минимум {length} символов." }; }
         public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "Пароль должен содержать хотя бы один знак пунктуации." }; }
         public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "Пароль должен иметь хотя бы одну цифру ('0'-'9')." }; }
